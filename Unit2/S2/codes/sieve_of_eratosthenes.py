5be254645d73c1f7e35cf2b7f84dfef66cbc9aac
SwarnaLathaNatesan <lathanatesan08@gmail.com>
2025-08-26 09:24:20 +0530

    i

---
+++ Unit2/S2/codes/sieve_of_eratosthenes.py
@@ -0,0 +1,14 @@
+def sieve_of_eratosthenes(n):
+    """Return a list of prime numbers up to n."""
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False # 0 and 1 are not primes
+    
+    for current_num in range(2, int(n**0.5) + 1):
+        if is_prime[current_num]:
+            # Mark multiples of current_num as composite
+            for multiple in range(current_num * current_num, n + 1, current_num):
+                is_prime[multiple] = False
+    
+    # Collect prime numbers
+    primes = [num for num, prime in enumerate(is_prime) if prime]
+    return primes
