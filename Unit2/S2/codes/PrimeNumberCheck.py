0e0d8f3076263a426e68d0455ebad7afa21fc1a4
SwarnaLathaNatesan <lathanatesan08@gmail.com>
2025-09-01 13:49:53 +0530

    upd

--- Unit2/S2/codes/PrimeNumberCheck.py
+++ Unit2/S2/codes/PrimeNumberCheck.py
@@ -1,24 +1,15 @@
 # Python program to check for prime number (optimized)
 def is_prime_optimized(n):
-
     if n <= 1: return False, 0
     if n == 2: return True, 0
     if n % 2 == 0: return False, 1
     iterations = 0
 
-
     for i in range(3, int(n**0.5) + 1, 2):
-
-		iterations += 1
-
+        iterations += 1
         if n % i == 0 :
-         return False, iterations
+            return False, iterations
     return True, iterations
-
-
-
-
-
 
 number = 1001
 result, iters = is_prime_optimized(number)
@@ -31,8 +22,8 @@ primeCounter = 0
 for i in range (2, 1001):
     result, iters = is_prime_optimized(i)
     totalIter = totalIter + iters
-    if result 
-	primeCounter = primeCounter+1
+    if result :
+	    primeCounter = primeCounter+1
 	
     print(f"{i} is prime: {result} , {primeCounter}")
 
