a535e3547348904d8a45eb92c185c1beb7abdc0b
SwarnaLathaNatesan <lathanatesan08@gmail.com>
2025-09-03 18:59:29 +0530

    upd

--- Unit2/S2/codes/findDivisor.py
+++ Unit2/S2/codes/findDivisor.py
@@ -1,12 +1,19 @@
 
 def find_divisors(n):
     divisors = []
+
     sqrtN = int( n ** .5)
     for i in range(1, sqrtN + 1):
         if n % i == 0:
             divisors.append(i)
-            divisors.append(int(n/i))
+            complementaryDivisor = int(n/i)
+            if (i != complementaryDivisor)	:	
+                divisors.append(complementaryDivisor)
     return divisors
+
+
+
+
 
 number = 36
 print(f"Divisors of {number}: {find_divisors(number)}")
