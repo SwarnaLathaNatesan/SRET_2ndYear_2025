05d870faf0ae13f0b627bee7e11492453c987007
SwarnaLathaNatesan <lathanatesan08@gmail.com>
2025-09-09 22:28:53 +0530

    upd

--- Unit2/S2/codes/PrimeNumberCheck.py
+++ Unit2/S2/codes/PrimeNumberCheck.py
@@ -1,30 +1,27 @@
 # Python program to check for prime number (optimized)
 def is_prime_optimized(n):
-    if n <= 1: return False, 0
-    if n == 2: return True, 0
-    if n % 2 == 0: return False, 1
-    iterations = 0
+    if n <= 1: return False
+    if n == 2: return True
+    if n % 2 == 0: return False
 
     for i in range(3, int(n**0.5) + 1, 2):
-        iterations += 1
+
         if n % i == 0 :
-            return False, iterations
-    return True, iterations
+            return False
+    return True
 
 number = 1001
-result, iters = is_prime_optimized(number)
+result = is_prime_optimized(number)
 print(f"{number} is prime: {result}")
-print(f"Iterations required: {iters}")
 
 
-totalIter = 0
+
+#inefficient method. instead use sieve of eratosthene 
 primeCounter = 0
 for i in range (2, 1001):
-    result, iters = is_prime_optimized(i)
-    totalIter = totalIter + iters
+    result = is_prime_optimized(i)
     if result :
 	    primeCounter = primeCounter+1
 	
-    print(f"{i} is prime: {result} , {primeCounter}")
-
-print(f" total Iterations required: {totalIter}")
+    print(f"{i} is prime: {result} ")
+print(f"Total prime numbers {primeCounter}")
