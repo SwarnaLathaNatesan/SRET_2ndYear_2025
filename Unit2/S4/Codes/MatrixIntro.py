3b92520dbe96c454d9f3e7a8420f7163c22bf071
SwarnaLathaNatesan <lathanatesan08@gmail.com>
2025-09-10 16:48:55 +0530

    Update MatrixIntro.py

--- Unit2/S4/Codes/MatrixIntro.py
+++ Unit2/S4/Codes/MatrixIntro.py
@@ -5,6 +5,9 @@ matrix = [
     [0, 0, 0]
 ]
 
+matrix[0][1] = 6
+matrix[2][2] = matrix[0][1] + matrix[0][1]
+
 print("Initial Matrix:")
 for row in matrix:
     print(row)
@@ -24,6 +27,9 @@ for i in range(len(matrix)):
  for j in range(len(matrix[i])) :
    matrix[i][j] =    matrix[i][j] + 5
 
+print("\nMatrix After all the element Updates:")
+for row in matrix:
+    print(row)
 
 
 for i in range(len(matrix)):
