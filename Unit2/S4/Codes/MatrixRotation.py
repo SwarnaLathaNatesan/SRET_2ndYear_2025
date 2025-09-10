2a8277dae854c894d3fa4ed914a68cf9994bf211
SwarnaLathaNatesan <lathanatesan08@gmail.com>
2025-09-10 09:43:48 +0530

    u

--- Unit2/S4/Codes/MatrixRotation.py
+++ Unit2/S4/Codes/MatrixRotation.py
@@ -5,11 +5,16 @@ def rotate_clockwise(matrix):
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
     
     # Reverse each row
     for i in range(n):
         matrix[i] = matrix[i][::-1]
     
+
+
+
+
     return matrix
 
 # Example usage
